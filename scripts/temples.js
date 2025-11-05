f63583e900a78417ab2896162c10515141cecd94
aprcleav <aprcleaver@gmail.com>
2025-11-05 14:40:26 -0800

    W2 temple picture album assignment

---
+++ scripts/temples.js
@@ -0,0 +1,15 @@
+// Hamburger menu
+const hamButton = document.querySelector("#menu");
+const mainNav = document.querySelector(".navigation");
+
+hamButton.addEventListener('click', () => {
+    mainNav.classList.toggle("open");
+    hamButton.classList.toggle("open");
+});
+
+// Footer
+const year = document.querySelector("#currentyear");
+const today = new Date();
+
+year.innerHTML = today.getFullYear();
+document.getElementById("lastModified").innerHTML = document.lastModified;
