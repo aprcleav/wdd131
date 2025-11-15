1028655917d0cb5cf6c233398f123a028169cb7d
aprcleav <aprcleaver@gmail.com>
2025-11-15 10:10:42 -0800

    added JS windChill function

--- scripts/place.js
+++ scripts/place.js
@@ -4,3 +4,18 @@ const today = new Date();
 
 year.innerHTML = today.getFullYear();
 document.getElementById("lastModified").innerHTML = document.lastModified;
+
+let temp = 5.5;
+let windSpeed = 33.7;
+
+function calculateWindChill(temp, windSpeed) {
+    if (temp <= 10 && windSpeed > 4.8) {
+        const windChill = 13.12 + (0.6215 * temp) - (11.37 * (windSpeed ** 0.16)) + ((0.3965 * temp) * (windSpeed ** 0.16))
+        return windChill
+    }
+    else {
+        return "N/A"
+    }
+}
+
+document.getElementById("chill").innerHTML = Number((calculateWindChill(temp, windSpeed).toFixed(1)));
