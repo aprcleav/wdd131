814d4437d78bb599ebbcf853db119ccd21445812
aprcleav <aprcleaver@gmail.com>
2025-11-28 15:02:37 -0800

    lighthouse fixes

--- scripts/form.js
+++ scripts/form.js
@@ -52,19 +52,3 @@ function populateProducts(products) {
         newProduct.appendChild(option);
     })
 }
-
-// ----------------- FORM SUBMISSION COUNTER ------------------
-
-const reviewsDisplay = document.querySelector(".numreviews");
-let numReviews = Number(localStorage.getItem("reviews-list")) || 0;
-
-if (numReviews !== 0) {
-    reviewsDisplay.textContent = numReviews;
-}
-else {
-    reviewsDisplay.textContent = `This is your first review! Thank you!`;
-}
-
-numReviews++;
-
-localStorage.setItem("reviews-list", numReviews);
